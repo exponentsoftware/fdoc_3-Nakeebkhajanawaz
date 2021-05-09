7a62580a8e55afeb1726c014f7e9e24bac585524
khajanawaz Nakeeb <76945260+Nakeebkhajanawaz@users.noreply.github.com>
2021-05-09 15:59:56 +0530

    day3

--- Q3likes.js
+++ Q3likes.js
@@ -1,3 +1,5 @@
+/*Create a function called likeProduct. This function will helps to like to the product if it is not liked and remove like if it was liked.*/
+
 const users = [
     {
             _id: 'ab12ex',
