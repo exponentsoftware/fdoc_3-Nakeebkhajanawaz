4637f71d735c5acc67a2cb2b8b494e9a226ee4bf
khajanawaz Nakeeb <76945260+Nakeebkhajanawaz@users.noreply.github.com>
2021-05-09 15:53:18 +0530

    Q1day1

--- arraytoobject.js
+++ arraytoobject.js
@@ -1,4 +1,7 @@
   
+/*A junior developer structure student name, skills and score in array of arrays which may not easy to read. Destruction the following array name to name,
+skills array to skills, scores array to scores, JavaScript score to jsScore and React score to reactScore variable.*/
+
 function ArraytoObject(array) {
 
     return array.map(person => Object.assign(...person.map(([k, v]) => ({ [k]: v }))));
