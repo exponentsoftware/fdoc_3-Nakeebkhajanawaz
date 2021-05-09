87397d2502328811da177211ae648097dfb48244
khajanawaz Nakeeb <76945260+Nakeebkhajanawaz@users.noreply.github.com>
2021-05-09 15:57:49 +0530

    day1

--- Q2Sign.js
+++ Q2Sign.js
@@ -1,3 +1,6 @@
+/*a. Create a function called ***signUp*** which allows user to add to the collection. If user exists, inform the user that he has already an account.
+b. Create a function called ***signIn*** which allows user to sign in to the application*/
+
 function signUp(name) {
     for (user of users) {
         if (name.username == name) {
