92ca037385fd61516b64504ef07ce1bfdb6d3c4d
khajanawaz Nakeeb <76945260+Nakeebkhajanawaz@users.noreply.github.com>
2021-05-09 15:58:46 +0530

    day3

--- Q2b.js
+++ Q2b.js
@@ -1,3 +1,10 @@
+
+/*The products array has three elements and each of them has six properties.
+
+a. Create a function called ***rateProduct*** which rates the product
+b. Create a function called ***averageRating*** which calculate the average rating of a product*/
+
+
 const users = [
     {
             _id: 'ab12ex',
